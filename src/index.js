658d370aeca6b4ac2751e4aa6ae16a48d669a5ff
ashwinbabus <ashwinbabus21@gmail.com>
2021-01-09 12:59:05 +0530

    hashrouter for netlify

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
+    <Router basename={process.env.PUBLIC_URL} >
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-    </BrowserRouter>
+    </Router>
   </Provider>,
 
   document.getElementById("root")
